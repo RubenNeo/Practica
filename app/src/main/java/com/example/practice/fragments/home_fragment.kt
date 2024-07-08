42d8817b10b0f73b186a91b03977477075d02742
Tu Nombre <tu@ejemplo.com>
2024-07-08 15:57:51 +0200

    I have only added the setonclicklistener in the details image

--- app/src/main/java/com/example/practice/fragments/home_fragment.kt
+++ app/src/main/java/com/example/practice/fragments/home_fragment.kt
@@ -1,5 +1,6 @@
 package com.example.practice.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -11,61 +12,66 @@ import com.example.practice.ApiServiceMeal.Meal
 import com.example.practice.ApiServiceMeal.MealList
 import com.example.practice.R
 import com.example.practice.Retrofit.RetrofitInstance
+import com.example.practice.activities.Details_Food_activity
 import com.example.practice.databinding.FragmentHomeFragmentBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-
 class home_fragment : Fragment() {
-    //Añadimos binding a la pantalla principal del fragment
+    // Añadimos binding a la pantalla principal del fragment
     private lateinit var binding: FragmentHomeFragmentBinding
 
-
     companion object {
-        const val MEAL_ID = "com.example.practice.fragments"
-        const val MEAL_NAME = "com.example.practice.fragments"
-        const val  MEAL_PHOTO = "com.example.practice.fragments"
-
+        const val MEAL_ID = "com.example.practice.fragments.MEAL_ID"
+        const val MEAL_NAME = "com.example.practice.fragments.MEAL_NAME"
+        const val MEAL_PHOTO = "com.example.practice.fragments.MEAL_PHOTO"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //Aqui añadimos el layout inflate del homeFragment
-        binding = FragmentHomeFragmentBinding.inflate(layoutInflater, container, false)
+        // Aqui añadimos el layout inflate del homeFragment
+        binding = FragmentHomeFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//Comprobamos que LLamamos bien tanto a la ID como al nombre de la API mediate un log.d
+
+        // Comprobamos que llamamos bien tanto a la ID como al nombre de la API mediante un log.d
         RetrofitInstance.api.getRandomFood().enqueue(object : Callback<MealList> {
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if (response.body() != null) {
                     val randomFood: Meal = response.body()!!.meals[0]
-                    //Añadimos el Glide para las imagenes random
+
+                    // Añadimos el Glide para las imágenes random
                     Glide.with(this@home_fragment)
                         .load(randomFood.strMealThumb)
                         .into(binding.randomFood)
+
+                    // Añadimos OnClickListener a la imagen
+                    binding.randomFood.setOnClickListener {
+                        val intent = Intent(activity, Details_Food_activity::class.java).apply {
+                            putExtra(MEAL_ID, randomFood.idMeal)
+                            putExtra(MEAL_NAME, randomFood.strMeal)
+                            putExtra(MEAL_PHOTO, randomFood.strMealThumb)
+                        }
+                        startActivity(intent)
+                    }
                 } else {
                     return
                 }
             }
 
-            override fun onFailure(p0: Call<MealList>, p1: Throwable) {
-                Log.d("Home Fragment", p1.message.orEmpty())
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("Home Fragment", t.message.orEmpty())
             }
-
         })
-
-
     }
 }
